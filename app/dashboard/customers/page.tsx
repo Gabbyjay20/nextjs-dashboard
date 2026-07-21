abc48e7b18010d8fa599395e54d88b9bd543dd9b
Gabriel James <gabbyjay20@gmai.com>
2026-07-21 21:53:12 +0100

    Customers page fixed

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,35 @@
 import type { Metadata } from 'next';
+import Search from '@/app/ui/search';
+import Table from '@/app/ui/customers/table';
+import { fetchFilteredCustomers } from '@/app/lib/data';
 
 export const metadata: Metadata = {
   title: 'Customers',
 };
 
-export default function Page() {
-  return <p>Customers Page</p>;
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: Promise<{
+    query?: string;
+  }>;
+}) {
+  const params = await searchParams;
+  const query = params?.query || '';
+
+  const customers = await fetchFilteredCustomers(query);
+
+  return (
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className="text-2xl">Customers</h1>
+      </div>
+
+      <div className="mt-4 mb-6">
+        <Search placeholder="Search customers..." />
+      </div>
+
+      <Table customers={customers} />
+    </div>
+  );
 }
